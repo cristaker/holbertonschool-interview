36b01663fa7739fd1464bb9e5db2a03f55619d40
cristian <cristaker1927@hotmail.com>
2023-01-10 16:38:43 -0500

    project

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void raise_error(void);
+int check_digits(char *num);
+void multiply_string(char *num1, char *num2, int len1, int len2);
+
+#endif
