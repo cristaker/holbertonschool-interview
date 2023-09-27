cc607e2352d6b622cf8dc468b32e4aa58e27fa8d
cristaker <christian.sanchezz@cadena.com.co>
2023-09-27 18:00:54 -0500

    project

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,50 +1,52 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line
-by line and computes metrics:
-"""
-from sys import stdin
 
-
-state_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
-size = 0
-
-
-def print_list():
-    """Total file size"""
-    print("File size: {}".format(size))
-    for key in sorted(state_code.keys()):
-        if state_code[key]:
-            print("{}: {}".format(key, state_code[key]))
-
+'''
+Reads stdin line by line and computes metrics
+'''
+import sys
 
 if __name__ == "__main__":
-    count = 0
+
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
+
+    def print_stats():
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
     try:
-        for line in stdin:
-            try:
-                getdata = line.split()
-                size += int(getdata[-1])
-                key_statucode = getdata[-2]
-                if key_statucode in state_code:
-                    state_code[key_statucode] += 1
-            except:
-                pass
-            if count == 9:
-                print_list()
-                count = -1
+        for line in sys.stdin:
+            parse_stdin(line)
+            # print the stats after every 10 outputs
+            if count % 10 == 0:
+                print_stats()
             count += 1
     except KeyboardInterrupt:
-        print_list()
+        print_stats()
         raise
-    print_list()
+    print_stats()
